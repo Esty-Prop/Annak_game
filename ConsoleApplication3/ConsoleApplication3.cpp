779b7c4643a4c41679e25f759f9eebd7c0ec53ba
Rachel5803 <r3115803@gmail.com>
2024-07-03 11:58:35 +0300

    try

--- ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -16,7 +16,7 @@ using json = nlohmann::json;
 int main() {
     cout << "esty";
     cout << "esty and racheli";
-    cout <<"hello world from Rachely"
+    cout << "hello world from Rachely";
     Game game;
     game.handleUserInput();
     game._world();
